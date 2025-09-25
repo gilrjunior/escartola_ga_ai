1aa511b6415d85ef6da5115b0648abf9ad72f9e5
Gilmar Júnior <gilmarjunioreis@gmail.com>
2025-09-25 14:03:03 -0300

    fix logo bug

--- config/settings.py
+++ config/settings.py
@@ -121,10 +121,12 @@ LANGUAGE_COOKIE_SECURE = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "config" / "public",
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
